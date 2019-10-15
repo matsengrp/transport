880a877fb52239c4b3900a04c907c03f6c905f1a
BrandenOlson <branden.olson@colorado.edu>
2019-10-15 08:19:36 -0700

    Compute runtime by sample size for sinkhorn analyses

--- bjo_analyses/complexity.py
+++ bjo_analyses/complexity.py
@@ -1,7 +1,9 @@
 import numpy as np
 import os
+import ot
 import pandas as pd
 import random
+import time
 
 def compute_runtimes(dist_mat_filename):
     pass
@@ -9,7 +11,7 @@ def compute_runtimes(dist_mat_filename):
 if __name__ == "__main__":
     dist_mat_filename = "Data/all_IELrep_beta_distances.txt"
     batch_filename = "Data/tmp_distances.tsv"
-    batch_sizes = [int(np.exp(x)) for x in range(3, 9)]
+    batch_sizes = [int(np.exp(x)) for x in range(3, 11)]
     for batch_size in batch_sizes:
         # First, subsample to batch_size rows using shuf, avoiding need to load full matrix into memory
         command = "shuf -n " + str(batch_size) + " " + dist_mat_filename + " > " + batch_filename
@@ -18,3 +20,11 @@ if __name__ == "__main__":
         num_columns = dist_mat.shape[1]
         # Next, subsample to batch_size columns
         dist_mat = dist_mat.iloc[:, random.sample(range(0, num_columns), batch_size)]
+
+        start_time = time.time()
+        lambdas = np.exp(np.linspace(-1 ,3, 20))
+        empirical_distribution = np.ones((batch_size,)) / batch_size
+        dists = [ot.sinkhorn2(empirical_distribution, empirical_distribution, dist_mat, lam) for lam in lambdas]
+        mats = [ot.sinkhorn(empirical_distribution, empirical_distribution, dist_mat, lam) for lam in lambdas]
+        elapsed_time = time.time() - start_time
+        print(str(batch_size) + ": " + str(elapsed_time))
