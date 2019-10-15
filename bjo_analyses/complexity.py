cd50857699c0777c7b7de4e314b1a26862cd2a59
BrandenOlson <branden.olson@colorado.edu>
2019-10-15 07:29:47 -0700

    Subsample to batch_size rows and columns for each iteration

--- bjo_analyses/complexity.py
+++ bjo_analyses/complexity.py
@@ -1,6 +1,14 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pandas as pd
+import random
+
+def plot_clonotype_empirical_distribution(filename):
+    clone_df = pd.read_csv(filename, sep="\t")
+    clone_sizes = clone_df['clone_size']
+    _ = plt.hist(clone_sizes, bins='auto')
+    plt.show()
 
 def compute_runtimes(dist_mat_filename):
     pass
@@ -9,8 +17,12 @@ def compute_runtimes(dist_mat_filename):
 if __name__ == "__main__":
     dist_mat_filename = "Data/all_IELrep_beta_distances.txt"
     batch_filename = "Data/tmp_distances.tsv"
-    batch_sizes = [np.floor(np.exp(x)) for x in range(3, 9)]
+    batch_sizes = [int(np.exp(x)) for x in range(3, 9)]
     for batch_size in batch_sizes:
+        # First, subsample to batch_size rows using shuf, avoiding need to load full matrix into memory
         command = "shuf -n " + str(batch_size) + " " + dist_mat_filename + " > " + batch_filename
         os.system(command)
         dist_mat = pd.read_csv(batch_filename, sep=" ", header=None)
+        num_columns = dist_mat.shape[1]
+        # Next, subsample to batch_size columns
+        dist_mat = dist_mat.iloc[:, random.sample(range(0, num_columns), batch_size)]
