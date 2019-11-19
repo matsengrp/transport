50976063b6e8538fbfc3e3bf3d188bd89a4f536c
BrandenOlson <branden.olson@colorado.edu>
2019-11-19 10:06:21 -0800

    clean up plots

--- plot_results.R
+++ plot_results.R
@@ -18,13 +18,21 @@ jaccards <- results %>%
     melt %>%
     data.table::setnames(c("Jaccard", "Group"))
 
-p1 <- ggplot(scores) + geom_density(aes(x=value, color=L1))
-p2 <- ggplot(jaccards) + geom_density(aes(x=value, color=L1))
+p1 <- ggplot(scores) + 
+    geom_density(aes(x=Score, color=Group), size=1) +
+    ggtitle("Max score by group") +
+    theme_minimal(base_size=14)
+p2 <- ggplot(jaccards) + 
+    geom_density(aes(x=Jaccard, color=Group), size=1)  +
+    ggtitle("Jaccard index by group") +
+    theme_minimal(base_size=14)
 
 merged <- cbind(scores, jaccards)[, 1:3]
 merged[["Fit"]] <- lm(Score ~ Jaccard, data=merged)[["fitted.values"]]
 p3 <- ggplot(merged, aes(x=Jaccard, y=Score, color=Group)) + 
     geom_point() +
-    geom_line(aes(x=Jaccard, y=Fit), color="black")
+    geom_line(aes(x=Jaccard, y=Fit), color="black", size=1) +
+    ggtitle("Max score versus Jaccard index (with least-squares fit)") +
+    theme_minimal(base_size=14)
 
 
