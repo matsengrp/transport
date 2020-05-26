a3b99490421a15788d374c69c7ed81fc5cd2e3c0
BrandenOlson <branden.olson@colorado.edu>
2020-05-26 14:24:06 -0700

    test hmmer evalues for example

--- test/test_hmmer_manager.py
+++ test/test_hmmer_manager.py
@@ -1,6 +1,8 @@
 import os
 import sys
 
+import numpy as np
+
 sys.path.append(os.getcwd())
 
 from python.hmmer_manager import HMMerManager
@@ -11,3 +13,4 @@ def test_hmmer():
     hmmer_manager.run_hmmbuild('tmp_output/test.hmm', 'tmp_output/test.sto')
     result = hmmer_manager.run_hmmsearch('tmp_output/test.hmm', 'tmp_output/test.sto', 'tmp_output/test.out')
     assert len(result) == 6
+    assert np.all([float(i['e_value']) < 1e-08 for i in result])
