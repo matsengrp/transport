ddb9b0a5d97cb72f21e2941f2c91e207eb0700ca
BrandenOlson <branden.olson@colorado.edu>
2020-06-20 14:49:18 -0700

    start iterative clustering procedure (and call R segreg from python)

--- analyses/combined_replicates.py
+++ analyses/combined_replicates.py
@@ -1,11 +1,14 @@
 from glob import glob
 import os
+import subprocess
 import sys
 
 import pandas as pd
 
 sys.path.append(os.getcwd())
-from common.params import DIRECTORIES, IEL_DATA_DIR, TMP_OUTPUT
+from common.params import CSV_OUTPUT_DIRNAME, DIRECTORIES, IEL_DATA_DIR, TMP_OUTPUT
+from python.tcr_clusterer import TCRClusterer
+from python.tcr_dist import TCRDist
 from python.tcr_scorer import TCRScorer
 from python.utils import get_df_from_file
 
@@ -21,10 +24,20 @@ cd4_reptag = "CD4"
 dn_reptag = "DN"
 chain = "B"
 
-dn_file = os.path.join(DIRECTORIES[TMP_OUTPUT], "all_dn.csv")
-cd4_file = os.path.join(DIRECTORIES[TMP_OUTPUT], "all_cd4.csv")
+dn_filename = "all_dn.csv"
+cd4_filename = "all_cd4.csv"
+dn_file = os.path.join(DIRECTORIES[TMP_OUTPUT], dn_filename)
+cd4_file = os.path.join(DIRECTORIES[TMP_OUTPUT], cd4_filename)
 write_full_replicate_dataset(dn_file, "DN")
 write_full_replicate_dataset(cd4_file, "CD4")
 
 scorer = TCRScorer(file_1=cd4_file, file_2=dn_file)
-import pdb; pdb.set_trace()
+dn_self_dist_mat = scorer.repertoire_2.distance_matrix
+tcr_clusterer = TCRClusterer(self_distance_matrix=dn_self_dist_mat, score_dict=scorer.enrichment_dict)
+seg_csv_file = os.path.join(CSV_OUTPUT_DIRNAME, "seg.csv")
+tcr_clusterer.df.loc[:, ('radius', 'annulus_enrichment')].to_csv(seg_csv_file, index=False)
+
+
+os.system('Rscript R/segmented_regression.R')
+with open('tmp_output/breakpoint.txt') as f:
+    bp = [float(line.strip()) for line in f]
