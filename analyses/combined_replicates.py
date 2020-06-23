031640218d8bbd1a33734e664271528655a5dc44
BrandenOlson <branden.olson@colorado.edu>
2020-06-23 10:28:02 -0700

    implement multi-cluster routine

--- analyses/combined_replicates.py
+++ analyses/combined_replicates.py
@@ -9,6 +9,7 @@ import pandas as pd
 
 sys.path.append(os.getcwd())
 from common.params import CSV_OUTPUT_DIRNAME, DIRECTORIES, IEL_DATA_DIR, JSON_OUTPUT, TMP_OUTPUT
+from python.hmmer_manager import HMMerManager
 from python.tcr_clusterer import TCRClusterer
 from python.tcr_dist import TCRDist
 from python.tcr_scorer import TCRScorer
@@ -33,17 +34,47 @@ cd4_file = os.path.join(DIRECTORIES[TMP_OUTPUT], cd4_filename)
 write_full_replicate_dataset(dn_file, "DN")
 write_full_replicate_dataset(cd4_file, "CD4")
 
-scorer = TCRScorer(file_1=cd4_file, file_2=dn_file)
-dn_self_dist_mat = scorer.repertoire_2.distance_matrix
-tcr_clusterer = TCRClusterer(self_distance_matrix=dn_self_dist_mat, score_dict=scorer.enrichment_dict)
-tcr_clusterer.cluster_df.to_csv(os.path.join(CSV_OUTPUT_DIRNAME, "combined_cluster_df.csv"))
+def run_clustering_step(file_1, file_2):
+    scorer = TCRScorer(file_1=file_1, file_2=file_2)
+    rep_2_self_dist_mat = scorer.repertoire_2.distance_matrix
+    tcr_clusterer = TCRClusterer(self_distance_matrix=rep_2_self_dist_mat, score_dict=scorer.enrichment_dict)
+    return scorer, tcr_clusterer
 
-np.savetxt(
-    os.path.join(
-        DIRECTORIES["dist_matrices"],
-        "full_dn.csv",
-    ),
-    dn_self_dist_mat,
-    delimiter=",",
-    fmt="%i"
-)
+initial_scorer = TCRScorer(file_1=cd4_file, file_2=dn_file)
+initial_rep_2_self_dist_mat = initial_scorer.repertoire_2.distance_matrix
+initial_tcr_clusterer = TCRClusterer(self_distance_matrix=initial_rep_2_self_dist_mat, score_dict=initial_scorer.enrichment_dict)
+result = {tcr: {'score': score, 'cluster': 0} for tcr, score in initial_scorer.enrichment_dict.items()} 
+for tcr in initial_tcr_clusterer.cluster_dict['tcrs']:
+    result[tcr]['cluster'] = 1
+
+sub_repertoire_tcrs = [tcr for tcr in initial_scorer.repertoire_2.unique_tcrs if tcr not in initial_tcr_clusterer.cluster_dict['tcrs']]
+cluster = 2
+sub_cd4_file = os.path.join(DIRECTORIES[TMP_OUTPUT], 'sub_cd4.csv')
+while cluster < 6:
+    np.savetxt(sub_cd4_file, sub_repertoire_tcrs, fmt="%s")
+    current_scorer, current_clusterer = run_clustering_step(file_1=cd4_file, file_2=sub_cd4_file) 
+    sub_repertoire_tcrs = [tcr for tcr in current_scorer.repertoire_2.unique_tcrs if tcr not in current_clusterer.cluster_dict['tcrs']]
+    for tcr in current_clusterer.cluster_dict['tcrs']:
+        result[tcr]['cluster'] = cluster
+    cluster = cluster + 1
+
+
+pd.DataFrame(result).transpose().to_csv(os.path.join(DIRECTORIES[TMP_OUTPUT], 'multiple_clusters.csv'))
+
+#np.savetxt(
+#    os.path.join(
+#        DIRECTORIES["dist_matrices"],
+#        "full_dn.csv",
+#    ),
+#    dn_self_dist_mat,
+#    delimiter=",",
+#    fmt="%i"
+#)
+
+sub_repertoire_tcrs = [tcr for tcr in scorer.repertoire_2.unique_tcrs if tcr not in tcr_clusterer.cluster_dict['tcrs']]
+sub_dn_file = os.path.join(DIRECTORIES[TMP_OUTPUT], "sub_dn.csv")
+scorer = TCRScorer(file_1=cd4_file, file_2=sub_dn_file)
+
+
+#hmmer_manager = HMMerManager()
+#hmmer_manager.build_hmm_from_sequences([s.split(',')[1] for s in tcr_clusterer.cluster_dict['tcrs']])
