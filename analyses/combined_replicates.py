04fd90af767e7b3d6b668b1a195cedbc93a925fd
BrandenOlson <branden.olson@colorado.edu>
2020-07-02 15:22:26 -0700

    fix cluster repeat issue

--- analyses/combined_replicates.py
+++ analyses/combined_replicates.py
@@ -34,15 +34,20 @@ cd4_file = os.path.join(DIRECTORIES[TMP_OUTPUT], cd4_filename)
 write_full_replicate_dataset(dn_file, "DN")
 write_full_replicate_dataset(cd4_file, "CD4")
 
-def run_clustering_step(file_1, file_2, cluster):
+def run_clustering_step(file_1, file_2, cluster, score_dict=None):
     scorer = TCRScorer(file_1=file_1, file_2=file_2)
+    if score_dict is None:
+        score_dict=scorer.enrichment_dict
     rep_2_self_dist_mat = scorer.repertoire_2.distance_matrix
-    tcr_clusterer = TCRClusterer(self_distance_matrix=rep_2_self_dist_mat, score_dict=scorer.enrichment_dict, cluster_label=f"cluster_{cluster}")
+    tcr_clusterer = TCRClusterer(self_distance_matrix=rep_2_self_dist_mat, score_dict=score_dict, cluster_label=f"cluster_{cluster}")
     return scorer, tcr_clusterer
+
+all_clusters = []
 
 initial_scorer = TCRScorer(file_1=cd4_file, file_2=dn_file)
 initial_rep_2_self_dist_mat = initial_scorer.repertoire_2.distance_matrix
-initial_clusterer = TCRClusterer(self_distance_matrix=initial_rep_2_self_dist_mat, score_dict=initial_scorer.enrichment_dict, cluster_label="cluster_1")
+initial_score_dict = initial_scorer.enrichment_dict
+initial_clusterer = TCRClusterer(self_distance_matrix=initial_rep_2_self_dist_mat, score_dict=initial_score_dict, cluster_label="cluster_1")
 result = {tcr: {'score': score, 'cluster': 0} for tcr, score in initial_scorer.enrichment_dict.items()} 
 for tcr in initial_clusterer.cluster_dict['tcrs']:
     result[tcr]['cluster'] = 1
@@ -60,13 +65,15 @@ hmmer_manager.build_hmm_from_sequences(
     plot_filename=os.path.join(DIRECTORIES[HMM_OUTPUT], 'cluster_1.png')
 )
 cluster = 2
-sub_cd4_file = os.path.join(DIRECTORIES[TMP_OUTPUT], 'sub_cd4.csv')
+sub_dn_file = os.path.join(DIRECTORIES[TMP_OUTPUT], 'sub_dn.csv')
 while cluster < 10:
-    np.savetxt(sub_cd4_file, sub_repertoire_tcrs, fmt="%s")
-    current_scorer, current_clusterer = run_clustering_step(file_1=cd4_file, file_2=sub_cd4_file, cluster=cluster) 
+    np.savetxt(sub_dn_file, sub_repertoire_tcrs, fmt="%s")
+    score_dict = {k: initial_score_dict[k] for k in sub_repertoire_tcrs}
+    current_scorer, current_clusterer = run_clustering_step(file_1=cd4_file, file_2=sub_dn_file, cluster=cluster, score_dict=score_dict) 
     sub_repertoire_tcrs = [tcr for tcr in current_scorer.repertoire_2.unique_tcrs if tcr not in current_clusterer.cluster_dict['tcrs']]
     for tcr in current_clusterer.cluster_dict['tcrs']:
         result[tcr]['cluster'] = cluster
+    hmmer_manager = HMMerManager()
     hmmer_manager.build_hmm_from_sequences(
         [s.split(',')[1] for s in current_clusterer.cluster_dict['tcrs']],
         hmm_filename=os.path.join(DIRECTORIES[HMM_OUTPUT], 'cluster_{}.hmm'.format(cluster)),
