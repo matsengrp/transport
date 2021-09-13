c389da0995df9ce2523420b3076caffc3b279591
jgallowa07 <jaredgalloway07@gmail.com>
2021-09-13 10:46:48 -0700

    cleanup for discussion

--- R/motif.R
+++ R/motif.R
@@ -28,28 +28,23 @@ pdf(file.path(motif_dir, "enrichment_by_radius.pdf"), width=10, height=12)
 par(mfrow=c(6, 4))
 motif_dat[["subject"]] <- motif_dat[["subject"]] %>% sapply(gsub, pattern=".tcrs", replacement="")
 # Enforce an ordering for the subjects for plotting
-print(subjects)
 subjects <- factor(subjects, levels=1:23) %>% paste("DN", ., "B", sep="_")
-print(subjects)
+#print(subjects)
 for(tmp_subject in subjects[order(subjects)]) {
-    #print(paste0(tmp_subject, '.tcrs'))
-    print(score_results[['results']]) 
-    #print(score_results[['results']][[paste0(tmp_subject, '.tcrs')]])
-    #d_sub <- motif_dat[motif_dat$subject == tmp_subject, ]
-    #sample_size <- length(score_results[['results']][[paste0(tmp_subject, '.tcrs')]][['48.5']])
-    #sample_size <- length(score_results[['results']][[paste0(tmp_subject, '.tcrs')]][['48.5']])
+    d_sub <- motif_dat[motif_dat$subject == tmp_subject, ]
+    sample_size <- length(score_results[['results']][[paste0(tmp_subject, '.tcrs')]][['48.5']])
     #print(sample_size)
-    #if(sample_size) {
-    #    print(tmp_subject)
-    #    lm_fit <- lm(annulus_enrichment ~ radius, data=d_sub)
-    #    seg_fit <- segmented(lm_fit, psi=list(radius=40.5))
-    #    breakpoint <- seg_fit %>% get_breakpoint_from_model
-    #    breakpoints <- c(breakpoints, breakpoint)
-    #    cutoff_radius <- radii[which(radii <= breakpoint) %>% max]
-    #    cluster_tcrs[[tmp_subject]] <- json_object[[tmp_subject]][[toString(cutoff_radius)]][["tcrs"]]
-    #    xs <- seq(0, max(d_sub$radius), length.out=1000)
-    #    plot(d_sub$annulus_enrichment ~ d_sub$radius, pch=19, xlab="Radius", ylab="Mean loneliness", main=gsub(gsub(tmp_subject, pattern="DN_", replacement="Subject "), pattern="_B", replacement=""))
-    #    lines(predict(seg_fit, newdata=data.frame(radius=xs)) ~ xs, col="red", lty=ifelse(sample_size > 200, 1, 2))
-    #}
+    if(sample_size) {
+        print(tmp_subject)
+        lm_fit <- lm(annulus_enrichment ~ radius, data=d_sub)
+        seg_fit <- segmented(lm_fit, psi=list(radius=40.5))
+        breakpoint <- seg_fit %>% get_breakpoint_from_model
+        breakpoints <- c(breakpoints, breakpoint)
+        cutoff_radius <- radii[which(radii <= breakpoint) %>% max]
+        cluster_tcrs[[tmp_subject]] <- json_object[[tmp_subject]][[toString(cutoff_radius)]][["tcrs"]]
+        xs <- seq(0, max(d_sub$radius), length.out=1000)
+        plot(d_sub$annulus_enrichment ~ d_sub$radius, pch=19, xlab="Radius", ylab="Mean loneliness", main=gsub(gsub(tmp_subject, pattern="DN_", replacement="Subject "), pattern="_B", replacement=""))
+        lines(predict(seg_fit, newdata=data.frame(radius=xs)) ~ xs, col="red", lty=ifelse(sample_size > 200, 1, 2))
+    }
 }
 dev.off()
